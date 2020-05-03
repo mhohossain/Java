dfea3a0e764c2eee0ee8ea8a3d88dd3158f4ec91
unknown <mhohossain@yahoo.com>
2020-05-03 11:56:04 -0400

    Added the duplicate code

---
+++ src/ArrayListDemo/LagestValue1.java
@@ -0,0 +1,23 @@
+package ArrayListDemo;
+
+public class LagestValue1 {
+
+	public static void main(String[] args) {
+		
+		int x=100;
+		int y=200;
+		int z=300;
+		
+		if((x>y) && (x>z)) {
+			System.out.println("X is the greatest");
+		}
+		else if((y>x) && (y>z)) {
+			System.out.println("Y is the greatest");
+		}
+		else {
+			System.out.println("Z is the greatest");
+		}
+
+	}
+
+}
